0f2ee591f86b6a76b9d0c2b774b34194dbbc4d21
Ryan Nageer <rfn22@fsu.edu>
2024-11-20 01:02:47 -0500

    Major design overhaul, added course history, added 4 side images, added new self portrait

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,22 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component'; // Adjust path if needed
 import { CommonModule } from '@angular/common';
 import { AboutmeComponent } from "./aboutme/aboutme.component";
+import { ExperienceComponent } from './experience/experience.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CommonModule, AboutmeComponent],
+  imports: [RouterOutlet, HeaderComponent, CommonModule, AboutmeComponent, ExperienceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'portfolio';
   imagePath: string = "images/wallpaper1.jpg";
+  goldelite: string = "images/gold_elite.png"
+  elite1: string = "images/elite1.jpeg"
+  halo2: string = "images/halo2.png"
+  arbiter: string = "images/arbiter.png"
+  noble6: string = "images/noble6.png"
 }
 
